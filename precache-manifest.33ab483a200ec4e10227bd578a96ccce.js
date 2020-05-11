660f13c9e07f41102cbaa4836a51b31e2f985413
Manas Kulkarni <61631964+Manas588@users.noreply.github.com>
2020-05-12 01:50:08 +0530

    Updates

---
+++ precache-manifest.33ab483a200ec4e10227bd578a96ccce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67b07a7fc63ee6d887ff1f1cf83c061f",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "fd4b25905123bed0aa66",
+    "url": "/smart-brain/static/css/2.dded9572.chunk.css"
+  },
+  {
+    "revision": "bc7b09b9bb7b5875acf9",
+    "url": "/smart-brain/static/css/main.2dc1af7a.chunk.css"
+  },
+  {
+    "revision": "fd4b25905123bed0aa66",
+    "url": "/smart-brain/static/js/2.e38b566b.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/smart-brain/static/js/2.e38b566b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc7b09b9bb7b5875acf9",
+    "url": "/smart-brain/static/js/main.dc904ba4.chunk.js"
+  },
+  {
+    "revision": "8aa1a42498245f9e5806",
+    "url": "/smart-brain/static/js/runtime-main.2cb7f337.js"
+  }
+]);
